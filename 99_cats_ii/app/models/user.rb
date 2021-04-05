b0ce71ed2773083772b35ce103278f3c01c7380b
chrisweeting <chrisweeting@gmail.com>
2021-04-05 14:51:41 -0400

    Create controllers

--- 99_cats_ii/app/models/user.rb
+++ 99_cats_ii/app/models/user.rb
@@ -16,6 +16,15 @@ class User < ApplicationRecord
 
     after_initialize :ensure_session_token
 
+    def self.find_by_credentials(username, password)
+        user = User.find_by(username: username)
+
+        if user && user.is_password?(password)
+            user
+        else
+            nil
+        end
+    end
 
     def reset_session_token!
 
@@ -35,6 +44,8 @@ class User < ApplicationRecord
         password_object = BCrypt::Password.new(self.password_digest)
         password_object.is_password?(password)
     end
+
+    
 
 
 end
