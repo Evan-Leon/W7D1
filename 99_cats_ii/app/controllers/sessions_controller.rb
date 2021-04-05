08fae9a66cd8c1ddd8ac5f87b669818480eaeeb0
chrisweeting <chrisweeting@gmail.com>
2021-04-05 15:27:42 -0400

    Session controll er

--- 99_cats_ii/app/controllers/sessions_controller.rb
+++ 99_cats_ii/app/controllers/sessions_controller.rb
@@ -6,11 +6,19 @@ class SessionsController < ApplicationController
     end
 
     def create
+        @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
 
+        if @user
+            login!(@user)
+            redirect_to cats_url
+        else
+            render :new
+        end
     end
 
     def destroy 
-
+        logout!
+        redirect_to new_session_url
     end
 
 end
